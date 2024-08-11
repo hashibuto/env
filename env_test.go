ddac61e4eb5676380f976eda6ad8345a5df1ba6a
= <=>
2024-08-11 11:39:26 +0900

    added maps

--- env_test.go
+++ env_test.go
@@ -11,16 +11,17 @@ import (
 type ApplicationConfig struct {
 	StringVariableDefault   string `env:"CONFIG_MY_STRING_DEFAULT,hello"`
 	NonVariable             string
-	StringVariableNoDefault string        `env:"CONFIG_MY_STRING"`
-	IntVar                  int           `env:"CONFIG_MY_INT"`
-	IntVarDef               int           `env:"CONFIG_MY_INT_DEFAULT,44"`
-	FloatVar                float32       `env:"CONFIG_MY_FLOAT,32.3"`
-	BoolVal                 bool          `env:"CONFIG_MY_BOOL"`
-	BoolValDefault          bool          `env:"CONFIG_MY_BOOL_DEFAULT,t"`
-	DurationM               time.Duration `env:"CONFIG_MY_MINUTES,10m,duration"`
-	DurationS               time.Duration `env:"CONFIG_MY_SECONDS,10s,duration"`
-	SliceOfStrings          []string      `env:"CONFIG_NAMES,james johnson|robin ramsey|silly squirrels"`
-	EmptySlice              []string      `env:"CONFIG_NO_NAMES"`
+	StringVariableNoDefault string         `env:"CONFIG_MY_STRING"`
+	IntVar                  int            `env:"CONFIG_MY_INT"`
+	IntVarDef               int            `env:"CONFIG_MY_INT_DEFAULT,44"`
+	FloatVar                float32        `env:"CONFIG_MY_FLOAT,32.3"`
+	BoolVal                 bool           `env:"CONFIG_MY_BOOL"`
+	BoolValDefault          bool           `env:"CONFIG_MY_BOOL_DEFAULT,t"`
+	DurationM               time.Duration  `env:"CONFIG_MY_MINUTES,10m,duration"`
+	DurationS               time.Duration  `env:"CONFIG_MY_SECONDS,10s,duration"`
+	SliceOfStrings          []string       `env:"CONFIG_NAMES,james johnson;robin ramsey;silly squirrels"`
+	EmptySlice              []string       `env:"CONFIG_NO_NAMES"`
+	MapValues               map[string]any `env:"CONFIG_NAMES_SET,james;john;silly"`
 }
 
 func TestEnv(t *testing.T) {
@@ -48,4 +49,7 @@ func TestEnv(t *testing.T) {
 	assert.Equal(t, config.SliceOfStrings[0], "james johnson")
 	assert.Equal(t, config.SliceOfStrings[1], "robin ramsey")
 	assert.Len(t, config.EmptySlice, 0)
+
+	_, ok := config.MapValues["john"]
+	assert.True(t, ok)
 }
